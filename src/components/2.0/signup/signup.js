fe2b42eeb415ce8b3665c90d8a77610acca7428e
rmd5 <rmd5@hw.ac.uk>
2022-05-24 16:18:40 +0200

    Add input validation

--- src/components/2.0/signup/signup.js
+++ src/components/2.0/signup/signup.js
@@ -5,6 +5,8 @@ import Count from '../countdown/countdown';
 import Background from '../format/background';
 import Logo from '../format/logo';
 import Form from './form';
+import * as EmailValidator from 'email-validator';
+import { isValidPhoneNumber } from 'libphonenumber-js'
 
 function SignUp(props) {
     useEffect(() => {
@@ -19,28 +21,44 @@ function SignUp(props) {
     }, [])
 
     const [formBody, setFormBody] = useState({})
+    const [error, setError] = useState([])
+
+    useEffect(() => {
+        if (error) setError([])
+    }, [formBody])
+
+    function verifyComms() {
+        let arr = []
+        if (!EmailValidator.validate(formBody.email_address)) arr.push("Invalid email address")
+        if (!isValidPhoneNumber(formBody.country_code + formBody.phone_number)) arr.push("Invalid phone number")
+        setError(arr)
+        if (arr.length === 0) return true
+        else return false
+    }
 
     async function signUp() {
-        let res = await agent.user.signup(formBody)
-        if (res.success) {
-            try {
-                localStorage.setItem("free:token", res.token)
-                history.push("/freebie")
-            } catch (err) {
-                console.log(err)
+        if (verifyComms()) {
+            let res = await agent.user.signup(formBody)
+            if (res.success) {
+                try {
+                    localStorage.setItem("free:token", res.token)
+                    history.push("/freebie")
+                } catch (err) {
+                    console.log(err)
+                }
+            } else {
+                if (res.message === "User already exists with that email or phone number") {
+                    let login = await agent.user.login(formBody)
+                    if (login.success) {
+                        try {
+                            localStorage.setItem("free:token", login.token)
+                            history.push("/freebie")
+                        } catch (err) {
+                            console.log(err)
+                        }
+                    } else console.log(login.message)
+                } else console.log(res.message)
             }
-        } else {
-            if (res.message === "User already exists with that email or phone number") {
-                let login = await agent.user.login(formBody)
-                if (login.success) {
-                    try {
-                        localStorage.setItem("free:token", login.token)
-                        history.push("/freebie")
-                    } catch (err) {
-                        console.log(err)
-                    }
-                } else console.log(login.message)
-            } else console.log(res.message)
         }
     }
 
@@ -58,6 +76,7 @@ function SignUp(props) {
                         <div className='button' onClick={signUp}>
                             Sign up
                         </div>
+                        {error.length > 0 ? error.length === 2 ? <div className='error'>Invalid email and phone number</div> : <div className='error'>{error[0]}</div> : null}
                     </div>
                 </div>
 
